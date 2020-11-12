b8bb172db5ef789830fc9175448019bb0229d8c9
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-11-12 14:14:04 -0700

    added ability to play over and over again

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
-answer = input("Hello, Would you like to play? (yes/no) ")
+while True:
+    answer = input("Hello, Would you like to play? (yes/no) ")
 
-if answer .lower() .strip() == "yes":
+    if answer .lower() .strip() == "yes":
 
-    answer = input(
-        "You're walking and reach a fork in the road, would you like to go left or right?").lower().strip()
-    if answer == "left":
         answer = input(
-            "You encounter a group of zombies, would you like to run or attack?")
-
-        if answer == "attack":
-            print("That was a bad idea, you're all alone. Game over")
-        else:
-            print("Good choice, you ran away and escaped from the zombies")
-
+            "You're walking and reach a fork in the road, would you like to go left or right?").lower().strip()
+        if answer == "left":
             answer = input(
-                "You run over a hill and see a car and a helicopter, which one would you like to take?")
+                "You encounter a group of zombies, would you like to run or attack?")
 
-            if answer == "helicopter":
-                print(
-                    "Bad choice, you don't know how to fly a helicopter, you crash into the hill and blow up. Game over")
+            if answer == "attack":
+                print("That was a bad idea, you're all alone. Game over")
             else:
+                print("Good choice, you ran away and escaped from the zombies")
+
+                answer = input(
+                    "You run over a hill and see a car and a helicopter, which one would you like to take?")
+
+                if answer == "helicopter":
+                    print(
+                        "Bad choice, you don't know how to fly a helicopter, you crash into the hill and blow up. Game over")
+                else:
+                    print(
+                        "You drive away and find a safe place to rest. Congratulations, you have won the game")
+
+        elif answer == "right":
+            answer = input(
+                "You go right and come across a little lake, would you like to swim across it or take a boat?")
+
+            if answer == "swim":
                 print(
-                    "You drive away and find a safe place to rest. Congratulations, you have won the game")
+                    "You start swimming and almost make it when a big fish comes to the surface and eats you. Game over")
 
-    elif answer == "right":
-        answer = input(
-            "You go right and come across a little lake, would you like to swim across it or take a boat?")
+            else:
+                print("Good choice, you make it across the lake and find a cherry tree")
 
-        if answer == "swim":
-            print("You start swimming and almost make it when a big fish comes to the surface and eats you. Game over")
+                answer = input(
+                    "Would you like to eat some cherries?")
+
+                if answer == "yes":
+                    print(
+                        "Oh no! The cherries have poisoned you. You die instantly. Game over")
+                else:
+                    print(
+                        "Good choice, you have reached the escape point and have won the game")
 
         else:
-            print("Good choice, you make it across the lake and find a cherry tree")
-
-            answer = input(
-                "Would you like to eat some cherries?")
-
-            if answer == "yes":
-                print(
-                    "Oh no! The cherries have poisoned you. You die instantly. Game over")
-            else:
-                print(
-                    "Good choice, you have reached the escape point and have won the game")
+            print("Invalid choice, game over")
 
     else:
-        print("Invalid choice, game over")
-
-else:
-    print("That's too bad, come back when you're ready to play")
+        print("That's too bad, come back when you're ready to play")
+        break
